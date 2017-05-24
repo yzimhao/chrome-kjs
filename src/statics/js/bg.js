2dad316a14d091e530d99cdeb3d21648df91198d
yzimhao <yzimhao@126.com>
2017-05-24 10:21:11 +0800

    options page

--- src/statics/js/bg.js
+++ src/statics/js/bg.js
@@ -1,9 +1,11 @@
 (function() {
-    var glo = {
-        contextMenus: 'kjs-menu-1',
-        iscreateMenu: false,
-        iscloseMenu: false
-    };
+
+    // 获取options选项
+    chrome.storage.sync.get(function(opts){
+        if(opts){
+            glo.opts = opts;
+        }
+    });
 
     chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         if (message.type == 'tabinfo') {
@@ -14,8 +16,12 @@
                     'icon': tabs.favIconUrl
                 });
             });
+        } else if(message.type == 'update_opts') {
+            // 主动修改配置项
+            glo.opts = message.data;
         }
     });
+
 
     var open_new_tab = function(url){
         chrome.tabs.create({
@@ -26,33 +32,31 @@
 
     // contextMenus
     var createContextMenus = function() {
-        if(!glo.iscreateMenu && !glo.iscloseMenu){
-            chrome.contextMenus.create({
-                id: glo.contextMenus,
-                title: chrome.i18n.getMessage("search"),
-                contexts: ["page", "frame", "selection", "link", "image", "video", "audio"],
-                documentUrlPatterns: ["http://*/*", "https://*/*", "ftp://*/*"]
-            }, function(){
+        if(glo.opts.search != '0'){
+            if(!glo.iscreateMenu){
+                chrome.contextMenus.create({
+                    id: glo.contextMenus,
+                    title: chrome.i18n.getMessage("search"+ glo.opts.search),
+                    contexts: ["page", "frame", "selection", "link", "image", "video", "audio"],
+                    documentUrlPatterns: ["http://*/*", "https://*/*", "ftp://*/*"]
+                }, function(){
 
-            });
-            glo.iscreateMenu = true;
+                });
+                glo.iscreateMenu = true;
+                glo.search.cur = glo.opts.search;
+            }
         }
+
     };
     var removeContextMenus = function(){
-        chrome.storage.sync.get(function(opts){
-            if(opts.search == 0){
-                glo.iscloseMenu = true;
-                glo.iscreateMenu = false;
-            }else{
-                glo.iscloseMenu = false;
-            }
-
-            if(glo.iscloseMenu){
-                chrome.contextMenus.remove(glo.contextMenus);
-            }
-
-            console.log(glo);
-        });
+        if(glo.opts.search == '0' && glo.iscreateMenu){
+            chrome.contextMenus.remove(glo.contextMenus);
+            glo.iscreateMenu = false;
+        }
+        if(glo.search.cur != glo.opts.search && glo.iscreateMenu){
+            chrome.contextMenus.remove(glo.contextMenus);
+            glo.iscreateMenu = false;
+        }
     };
 
     chrome.tabs.onCreated.addListener(function(){
@@ -62,9 +66,13 @@
 
     chrome.contextMenus.onClicked.addListener(function(a, b){
         if(a.menuItemId === glo.contextMenus){
-            // 选中文字，打开百度搜索
+            // 选中文字，打开搜索
             if(a.selectionText && a.selectionText.trim() != ''){
-                open_new_tab("https://www.baidu.com/s?wd=" + encodeURIComponent(a.selectionText));
+                if(glo.opts.search=="1"){
+                    open_new_tab("https://www.baidu.com/s?wd=" + encodeURIComponent(a.selectionText));
+                }else if(glo.opts.search == "2"){
+                    open_new_tab("https://www.google.com/search?q=" + encodeURIComponent(a.selectionText));
+                }
             }
         }
     });
@@ -73,19 +81,11 @@
 
     // m3u8
     chrome.webRequest.onBeforeRequest.addListener(function(info){
-            // var enable = true;
-            chrome.storage.sync.get(function(opts){
-                if(opts.playhls){
-                    var playerurl = chrome.extension.getURL('player.html') + "?" + encodeURIComponent(info.url);
-                    chrome.tabs.update(info.tabId, {url: playerurl});
-                }
-            });
-
-            // if(enable){
-            //     var playerurl = chrome.extension.getURL('player.html') + "?" + encodeURIComponent(info.url);
-            //     chrome.tabs.update(info.tabId, {url: playerurl});
-            //     return {cancel: true}
-            // }
+            if(glo.opts.playhls){
+                var playerurl = chrome.extension.getURL('player.html') + "?" + encodeURIComponent(info.url);
+                chrome.tabs.update(info.tabId, {url: playerurl});
+                return {cancel: true};
+            }
         },
         {urls: ["*://*/*.m3u8*"], types:["main_frame"]},
         ['blocking']
--- src/statics/js/options.js
+++ src/statics/js/options.js
@@ -1,31 +1,32 @@
+console.log(glo.opts);
+
 // Saves options to chrome.storage
 function save_options() {
-  var search = document.getElementById('search').value;
-  var playhls = document.getElementById('playhls').checked;
-  chrome.storage.sync.set({
-    search: search,
-    playhls: playhls
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    var opts = {
+        search: document.getElementById('search').value,
+        playhls: document.getElementById('playhls').checked
+    };
+
+    chrome.storage.sync.set(opts, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+
+        // 配置被修改，主动通知bg.js
+        chrome.runtime.sendMessage({'type': 'update_opts', data: opts});
+    });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    search: 1, //默认百度
-    playhls: true
-  }, function(items) {
-    document.getElementById('search').value = items.search;
-    document.getElementById('playhls').checked = items.playhls;
-  });
+    chrome.storage.sync.get(glo.opts, function(items) {
+        document.getElementById('search').value = items.search;
+        document.getElementById('playhls').checked = items.playhls;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
